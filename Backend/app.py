01bf280c0e508a994be69d5697bdfa5be1687996
Ethan Willinger <EthanWillinger@my.unt.edu>
2023-10-07 17:58:00 -0500

    added db models

--- Backend/app.py
+++ Backend/app.py
@@ -4,6 +4,10 @@ from datetime import datetime, date
 
 app = Flask(__name__)
 
+#Add Databases
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
