ac6c11a5680345c0383eeca83a91e40326bc7ae1
Ethan Willinger <EthanWillinger@my.unt.edu>
2023-10-12 15:39:48 -0500

    Basic user registration on the backend can now add users to the user database, it is incredibly basic with no security, but it works

--- Backend/app.py
+++ Backend/app.py
@@ -9,6 +9,7 @@ import jwt
 
 
 app = Flask(__name__)
+api = Api(app)
 
 #Add Databases
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -26,9 +27,7 @@ with app.app_context():
     db.create_all()
 
 
-rest_api = Api()
-
-signup_model = rest_api.model('SignUpModel', {"username": fields.String(required=True, min_length=2, max_length=32),
+signup_model = api.model('SignUpModel', {"username": fields.String(required=True, min_length=2, max_length=32),
                                               "email": fields.String(required=True, min_length=4, max_length=64),
                                               "password": fields.String(required=True, min_length=4, max_length=16)
                                               })
@@ -37,23 +36,24 @@ signup_model = rest_api.model('SignUpModel', {"username": fields.String(required
 def hello():
     return "HELLO!"
 
-@rest_api.route('/api/users/register', endpoint = 'register')
+@api.route('/api/users/register', endpoint = 'register')
 class Register(Resource):
-    @rest_api.expect(signup_model, validate=True)
+    @api.expect(signup_model, validate=True)
     def post(self):
 
         req_data = request.get_json()
+        print(req_data)
 
         _username = req_data.get("username")
         _email = req_data.get("email")
         _password = req_data.get("password")
 
-        user_exists = Users.get_by_email(_email)
+        user_exists = Users.query.filter_by(email=_email).first()
         if user_exists:
             return {"success": False,
                     "msg": "Email already taken"}, 400
         
-        new_user = Users(1, username=_username, email=_email, password=_password)
+        new_user = Users(username=_username, email=_email, password=_password)
 
         db.session.add(new_user)
         db.session.commit()
