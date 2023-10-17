b65db5d69dc017e1dfed4c547c6951270e011226
Samuel Alvizo <34013376+Samtheman773@users.noreply.github.com>
2023-10-17 15:21:25 -0500

    Added login authentication code

--- Backend/app.py
+++ Backend/app.py
@@ -61,3 +61,28 @@ class Register(Resource):
         return {"success": True,
                 "userID": new_user.id,
                 "msg": "The user was successfully registered"}, 200
+
+
+# Login backend code
+login_model = api.model('LoginModel', {"email": fields.String(required=True,min_length=4, max_length=64), 
+                                       "password": fields.String(required=True, min_length=4, max_legnth=16)})
+# Note for UI: Max password length 16 characters
+@api.route('api/users/login', endpoint='login')
+class Login(Resource):
+    @api.expect(login_model, validate=True)
+    def post(self):
+        req_data = request.get_json()
+        _email = req_data.get('email')
+        _password = req_data.get('password')
+
+
+        # Check if the email exists in the database
+        user = Users.query.filter_by(email=_email).first()
+
+        # if both email and password are present, then check if the password is the same with the one in database
+        if user and user.password == _password:
+            #Generate a JSON web token for authentication
+            token = jwt.encode({"user_id": user.id}, 'secret_key', algorithm='HS256')
+            return {'success':True, 'token':token.decode('utf-8'), "msg":"login successful!"}, 200
+        else:
+            return {"success":False, "msg":"Invalid email or password"}, 401
