8cb40fa979cdd4850f27434d02744287d39c572d
OzBirdog <ozbirdett@gmail.com>
2023-11-11 20:49:22 -0600

    Fixed my CSS kinda

--- Frontend/src/navbar.jsx
+++ Frontend/src/navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginButton from './LoginButton';
+import AddListingButton from './AddListingButton';
 import './Navbar.css';
 
 export default function Navbar() {
     return (
         <nav className='navbar'>
             <LoginButton/>
+            <AddListingButton/>
             <h1>Garage Sale Finder</h1>
         </nav>
     );
