a278b1287a7c186f89d7aea5c95934de1353caea
Ethan Willinger <EthanWillinger@my.unt.edu>
2023-11-25 16:21:07 -0600

    Frontend now sends data to the backend and the backend returns with no errors, further testing will be needed to ensure that the coordinates are saved to db properly

--- Backend/app.py
+++ Backend/app.py
@@ -52,9 +52,8 @@ garagesale_model = api.model('GarageSaleModel', {
                                                 "end_date": fields.String(required=True, min_length=4, max_length=12),
                                                 "open_time": fields.String(required=True, min_length=4, max_length=12),
                                                 "close_time": fields.String(required=True, min_length=4, max_length=12),
-                                                "description": fields.String(required=True, min_length=4, max_length=500),
-                                                "latitude": fields.String(required = True, min_length = 3, max_length = 30),
-                                                "longitude": fields.String(required = True, min_length = 3, max_length = 30),
+                                                "description": fields.String(required=False, min_length=4, max_length=500),
+                                                
                                               })
 
 signup_model = api.model('SignUpModel', {"username": fields.String(required=True, min_length=2, max_length=32),
@@ -72,7 +71,7 @@ def hello():
     return "HELLO!"
 
 # Add garage sale entry
-@api.route('/api/garagesales/register', endpoint = 'garagesales_register')
+@api.route('/api/garagesales/register', endpoint = 'garagesale_register')
 class GarageSalesRegister(Resource):
 
     @api.expect(garagesale_model, validate=True)
@@ -81,46 +80,40 @@ class GarageSalesRegister(Resource):
         # Get JSON string submitted by user
         req_data = request.get_json()
 
-        if req_data.get("jwt") is None:
-            # Return HTTP code 401 (unauthorized) upon completion. 
-                return {"success": False,
-                        "msg": "Unauthorized!"}, 401
+
         
-        else:
-            token = req_data.get("jwt")
-            
-            #Take values from specified JSON keys and get the user_id from jwt token
-            _street_address = req_data.get("street_address")
-            _state = req_data.get("state")
-            _city = req_data.get("city")
-            _zip_code = req_data.get("zip_code")
-            _user_id = extract_id(token, keys[1])
-            _start_date = req_data.get("start_date")
-            _end_date = req_data.get("end_date")
-            _open_time = req_data.get("open_time")
-            _close_time = req_data.get("close_time")
-            _description = req_data.get("description")
-
-            locationInfo = ObtainGeoCodingApiData(_street_address, _city, _state, _zip_code, keys[2])
-            coordinates = ObtainCoordinates(locationInfo)
+        #Take values from specified JSON keys and get the user_id from jwt token
+        _street_address = req_data.get("street_address")
+        _state = req_data.get("state")
+        _city = req_data.get("city")
+        _zip_code = req_data.get("zip_code")
+        _user_id = req_data.get("user_id")
+        _start_date = req_data.get("start_date")
+        _end_date = req_data.get("end_date")
+        _open_time = req_data.get("open_time")
+        _close_time = req_data.get("close_time")
+        _description = req_data.get("description")
+        print(keys[2])
+        locationInfo = ObtainGeoCodingApiData(_street_address, _city, _state, _zip_code, keys[2])
+        coordinates = ObtainCoordinates(locationInfo)
 
 
-            # Make a new garage sale object from the values specified above
-            new_sale = GarageSales(street_adress = _street_address,
-                                   state = _state, city = _city, _zip_code = _zip_code,
-                                     user_id = _user_id, start_date = _start_date,
-                                       end_date = _end_date, open_time = _open_time,
-                                         close_time = _close_time, description = _description,
-                                         latitude = coordinates[0], longitude = coordinates[1])
+        # Make a new garage sale object from the values specified above
+        new_sale = GarageSales(street_address = _street_address,
+                                state = _state, city = _city, zip_code = _zip_code,
+                                    user_id = _user_id, start_date = _start_date,
+                                    end_date = _end_date, open_time = _open_time,
+                                        close_time = _close_time, description = _description,
+                                        latitude = coordinates[0], longitude = coordinates[1])
 
-            # Perform insertion query to GarageSales table and finalize query.
-            db.session.add(new_sale)
-            db.session.commit()
+        # Perform insertion query to GarageSales table and finalize query.
+        db.session.add(new_sale)
+        db.session.commit()
 
-            # Return HTTP code 200 (success) upon completion. 
-            return {"success": True,
-                    "garageSaleID": new_sale.id,
-                    "msg": "The garage sale was successfully registered"}, 200
+        # Return HTTP code 200 (success) upon completion. 
+        return {"success": True,
+                "garageSaleID": new_sale.id,
+                "msg": "The garage sale was successfully registered"}, 200
 
 
 
