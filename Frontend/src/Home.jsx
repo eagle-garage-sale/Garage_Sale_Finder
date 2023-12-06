0efc95cec20d569529fac3e000e88c73a7568bd9
Ethan <ethanrwillinger@gmail.com>
2023-12-06 15:18:19 -0600

    Props are now passed into lisiting components!

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -5,16 +5,36 @@ import Navbar from './Navbar'
 import LoginButton from './LoginButton';
 import AddListingButton from './AddListingButton';
 import * as Components from './Home_Components';
-import GetListingJSON  from './DisplayListings';
+import listing from './Listing_Component';
+import GetListingJSON from './utils/GetListings';
+import buildObjectArray from './utils/BuildListingArray';
+import ShowListing from './Listing_Component';
 
 
 
 export default function Home() {
+
+  //Get our listings from backend and build a readable object array
+  //whose contents will be passes as props in collection.map
   GetListingJSON();
+  const collection = buildObjectArray();
+
+  const listings = collection.map(function(item) {
+
+    return (
+      <ShowListing
+       key = {item.id}
+       {...item}/>
+    )
+  })
+
+
   return (
     <div className="home-text">
+      
       <Navbar/>
       <div className="left-column">
+
         <ul className='listing'>
           <li className='listing-item'>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJWX3CaS8jyYQkZmROfA-iwgDza17vjT4vlBJP6WshmCJ_kRx2793s4epRhkAmPwGkbtw&usqp=CAU" alt="Restaurant" />
@@ -25,6 +45,7 @@ export default function Home() {
             </div>
           </li>
         </ul>
+              {listings}
       </div>  
       <div className='right-column'>
         <Maps />
--- Frontend/src/Listing_Component.jsx
+++ Frontend/src/Listing_Component.jsx
@@ -1,37 +1,12 @@
 import React, { Component, useState} from "react";
 import * as Components from './Login_Components';
 
-
-function GetListingJSON() {
-    fetch('http://127.0.0.1:5000/api/home/sales', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        console.log(data.sales);
-        let ListingJSON = data.sales;
-        return ListingJSON;
-      } else {
-        console.error(data.msg);
-      }
-    })
-    .catch(error => {
-      console.error(error);
-    });
-
-};
-
 export default function ShowListing(props)
 {
-    const displayListings = listings.map((props) => (
+    return (
         <div className='listing-details' key={props.id}>
             <h3>
-                {props.id} {props.description}
+                 {props.description}
             </h3>
             <p>
                 Address: {props.street_address}, {props.state}
@@ -43,9 +18,8 @@ export default function ShowListing(props)
                 Open From: {props.open_time} - {props.close_time}
             </p>
         </div>
-    ));
+    );
 
-    return <div>{displayListings}</div>;
 }
 
 /*"{"id": 1,
