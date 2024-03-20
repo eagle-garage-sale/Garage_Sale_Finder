c7446f9d9a8d5f5d0415670602b3f41d70bcc3a8
Robert S <robertstephens5@my.unt.edu>
2024-03-20 15:16:15 -0500

    merged edit listing.

--- Frontend/src/AddListing.jsx
+++ Frontend/src/AddListing.jsx
@@ -1,13 +1,10 @@
-import React, {Component, useState} from "react";
-import { render } from 'react-dom';
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 import { KEYWORDS } from './keywords';
 import './tagstyle.css';
 import * as Components from './AddListing_Components';
 import { WithContext as ReactTags } from 'react-tag-input';
 import profanity from 'leo-profanity';
-import axios from "axios";
-import * as FormData from "form-data";
-
 
 function getDate() {
     const today = new Date();
@@ -30,15 +27,7 @@ function AddListing() {
     const [tags, setTags] = useState([]);
     const [errors, setErrors] = useState({});
 
-    //File states
-    const [files, setFiles] = useState(null);
-    const [ progress, setProgress ] = useState({ started: false, pc: 0 });
-    const [ msg, setMsg ] = useState(null);
-
-
     const navigate = useNavigate();
-    const [errorMessage, setErrorMessage] = useState('');
-
 
     const states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL',
                      'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME',
@@ -113,8 +102,6 @@ function AddListing() {
         return errors;
     }
 
-
-
     const handleDelete = (i) => {
         setTags(tags.filter((tag, index) => index !== i));
     };
@@ -124,26 +111,24 @@ function AddListing() {
             // Add the tag only if it's in the suggestions
             setTags([...tags, tag]);
           }
-  };
+    };
 
-  const handleDrag = (tag, currPos, newPos) => {
-    const newTags = tags.slice();
+    const handleDrag = (tag, currPos, newPos) => {
+        const newTags = tags.slice();
 
-    newTags.splice(currPos, 1);
-    newTags.splice(newPos, 0, tag);
+        newTags.splice(currPos, 1);
+        newTags.splice(newPos, 0, tag);
 
-    // re-render
-    setTags(newTags);
-  };
+        // re-render
+        setTags(newTags);
+    };
 
     const handleTagClick = (index) => {
         console.log('The tag at index ' + index + ' was clicked');
     };
-
-
+    
     const handleButtonClick = (event) => {
         event.preventDefault();
-        
         const errors = validateValues({
             streetAddress,
             state,
@@ -155,14 +140,11 @@ function AddListing() {
             closeTime,
             description
         });
-  const handleTagClick = (index) => {
-    console.log('The tag at index ' + index + ' was clicked');
-  };
-    
-    const handleButtonClick = () => {
 
+        if (Object.keys(errors).length == 0) {
+            console.log("Button clicked");
 
-        setErrorMessage('');   
+            const tagsString = tags.length > 0 ? tags.map(tag => tag.text).join(',') : "None";
 
             const garageData = {
                 street_address: streetAddress,
@@ -175,43 +157,8 @@ function AddListing() {
                 close_time: closeTime,
                 description: description,
                 tag: tagsString,
-                token: document.cookie,
+                token: document.cookie
             }
-
-            if (!files) {
-                setMsg("No file selected");
-                return;
-            }
-    
-            const fd = new FormData();
-            for (let i = 0; i<files.length; i++) {
-                fd.append("file", files[i]);
-            }
-    
-            fd.append("username", sessionStorage.getItem("username"));
-    
-            setMsg("Uploading...");
-            setProgress(prevState => {
-                return {...prevState, started: true}
-            })
-            axios.post('http://127.0.0.1:5000/garagesales/addImage', fd, {
-    
-                onUploadProgress: (progressEvent) => { setProgress(prevState => {
-                    return {...prevState, pc: progressEvent.progress*100}
-                })},
-                headers: {
-    
-                }          
-            })
-            .then(res => {
-                setMsg("Upload successful");
-                console.log(res.data)
-            })
-            .catch(err => {
-                setMsg("Upload failed");
-                console.error(err)
-            });
-    
             fetch('http://127.0.0.1:5000/api/garagesales/register', {
                 method: 'POST',
                 headers: {
@@ -224,7 +171,6 @@ function AddListing() {
                 if(data.success) {
                     console.log('Registration successful');
                     window.location.reload();
-
                 } else {
                     console.error(data.msg);
                 }
@@ -232,55 +178,11 @@ function AddListing() {
             .catch(error => {
                 console.error(error);
             });
-            navigate('/home');
-
-        if (streetAddress.trim() === '') {
-            setErrorMessage('Please enter a street address');
-            return; // Stop further execution if there's an error
+            navigate('/');
         }
-        if (state.trim() === '') {
-            setErrorMessage('Please enter a state');
-            return;
-        }
-     
-        console.log("Button clicked");
 
-        const tagsString = tags.map(tag => tag.text).join(',');
-
-        const garageData = {
-            street_address: streetAddress,
-            state: state,
-            city: city,
-            zip_code: zipcode,
-            start_date: startDate,
-            end_date: endDate,
-            open_time: openTime,
-            close_time: closeTime,
-            description: description,
-            tag: tagsString,
-            token: document.cookie
-        }
-        fetch('http://127.0.0.1:5000/api/garagesales/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(garageData)
-        })
-        .then(response => response.json())
-        .then(data => {
-            if(data.success) {
-                console.log('Registration successful');
-                window.location.reload();
-            } else {
-                console.error(data.msg);
-            }
-        })
-        .catch(error => {
-            console.error(error);
-        });
+        setErrors(errors);
     };
-
 
     return (
         <div className="form-text">
@@ -288,31 +190,33 @@ function AddListing() {
             <Components.header>Add Garage Sale Listing</Components.header>
 
             <Components.CenteredWrapper>
-        
+
             <Components.Container>
                 <Components.Form>
-
                     <Components.Title>Address</Components.Title>
                     <Components.AddressInput type='Street Address' placeholder='Street Address' value = {streetAddress} onChange={(e) => setStreetAddress(e.target.value)}/>
-                    {errorMessage && <div style={{ color: 'red', marginTop: '1px' }}>{errorMessage}</div>}
+                    {errors.streetAddress && <div style={{ color: 'red', marginTop: '1px' }}>{errors.streetAddress}</div>}
                     <Components.Select type='State' placeholder='State' value = {state} onChange={(e) => setState(e.target.value)}>
-                    {errorMessage && <div style={{ color: 'red', marginTop: '1px' }}>{errorMessage}</div>}
                         <option value ='' disabled>State</option>
                         {states.map((stateOption, index) => (
                              <option key={index} value={stateOption}>{stateOption}</option>
                              ))}
-                    </Components.Select>        
+                    </Components.Select>
+                    {errors.state && <div style={{ color: 'red', marginTop: '1px' }}>{errors.state}</div>}        
                     <Components.CityInput type='City' placeholder='City' value = {city} onChange={(e) => setCity(e.target.value)}/>
-                    <Components.ZipCodeInput type='Zipcode' placeholder='Zipcode' value = {zipcode} onChange={(e) => setZipcode(e.target.value)}/>
-
+                    {errors.city && <div style={{ color: 'red', marginTop: '1px' }}>{errors.city}</div>}
+                    <Components.ZipCodeInput type='Zipcode' placeholder='Zipcode' maxLength={5} value = {zipcode} onChange={(e) => setZipcode(e.target.value)}/>
+                    {errors.zipcode && <div style={{ color: 'red', marginTop: '1px' }}>{errors.zipcode}</div>}
                     <Components.DoubleContainer>
                         <div>
                             <Components.Title>Start Date</Components.Title>
                             <Components.DateInput type='date' value = {startDate} onChange={(e) => setStartDate(e.target.value)}/>
+                            {errors.startDate && <div style={{ color: 'red', marginTop: '1px' }}>{errors.startDate}</div>}
                         </div>
                         <div>
                             <Components.Title>End Date</Components.Title>
                             <Components.DateInput type='date' value = {endDate} onChange={(e) => setEndDate(e.target.value)}/>
+                            {errors.endDate && <div style={{ color: 'red', marginTop: '1px' }}>{errors.endDate}</div>}
                         </div>
                     </Components.DoubleContainer>
 
@@ -320,15 +224,18 @@ function AddListing() {
                         <div>
                             <Components.Title>Opening Time</Components.Title>
                             <Components.TimeInput type='time' value = {openTime} onChange={(e) => setOpenTime(e.target.value)}/>
+                            {errors.openTime && <div style={{ color: 'red', marginTop: '1px' }}>{errors.openTime}</div>}
                         </div>
                         <div>
                             <Components.Title>Closing Time</Components.Title>
                             <Components.TimeInput type='time' value = {closeTime} onChange={(e) => setCloseTime(e.target.value)}/>
+                            {errors.closeTime && <div style={{ color: 'red', marginTop: '1px' }}>{errors.closeTime}</div>}
                         </div>
                     </Components.DoubleContainer>
                     <Components.Title>Description</Components.Title>
                     <Components.DescriptionInput type='description' placeholder='500 Characters Max' value = {description} onChange={(e) => setDescription(e.target.value)}/>
-                    <Components.Title>Keywords</Components.Title>
+                    {errors.description && <div style={{ color: 'red', marginTop: '1px' }}>{errors.description}</div>}
+                    <Components.Title>Keywords (Recommended)</Components.Title>
                     <ReactTags
                        tags={tags}
                        suggestions={suggestions}
@@ -340,22 +247,16 @@ function AddListing() {
                        inputFieldPosition="bottom"
                        autocomplete
                         />
-
-                    <input onChange={ (e) => { setFiles(e.target.files) }} type="file" multiple/>
-                    <Components.Button
-                    onClick = {handleButtonClick}>
+                    <Components.Button onClick={handleButtonClick}>
                     Add Listing
                     </Components.Button>
-                    { progress.started && <progress max="100" value={progress.pc}></progress>}
-                    { msg && <span>{msg}</span>}
-                    
                 </Components.Form>
             </Components.Container>
             
             
             </Components.CenteredWrapper>
         </div>
-    );
+    )
 }
 
 export default AddListing;
